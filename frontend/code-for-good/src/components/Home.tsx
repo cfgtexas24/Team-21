96a0859ee22d0a7858111493962c102a3e8881f4
Ashley <ashleyoftexas@gmail.com>
2024-10-18 18:16:55 -0500

    linking home with login

--- frontend/code-for-good/src/components/Home.tsx
+++ frontend/code-for-good/src/components/Home.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../index.css";
+import { Button } from "@chakra-ui/react";
 
 const Home = () => {
   return (
     <div className="flex flex-col gap-6">
       <p className="text-3xl underline">STORM</p>
       <div>
-        <button>Login</button>
+        <Link to="/login">
+          <Button>Login</Button>
+        </Link>
       </div>
       <div>
         <Link to="/housing_form">
-          <button>Emergency Housing Form</button>
+          <Button>Emergency Housing Form</Button>
         </Link>
       </div>
     </div>
