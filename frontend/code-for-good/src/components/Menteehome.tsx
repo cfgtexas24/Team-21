66720f7bc72068ce3b9103a0ada1873216309d83
Austin Zaid Karimi <109770150+Austinzkarimi@users.noreply.github.com>
2024-10-19 01:26:04 -0500

    fixed bug

--- frontend/code-for-good/src/components/Menteehome.tsx
+++ frontend/code-for-good/src/components/Menteehome.tsx
@@ -11,7 +11,6 @@ import {
 } from "@chakra-ui/react";
 import { Card, CardBody, CardFooter } from "@chakra-ui/react";
 import Navbar from "./Navbar";
-import { Link } from "react-router-dom";
 
 const MenteeHome = () => {
   const handleVideoChat = () => {
