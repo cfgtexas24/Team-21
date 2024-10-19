6d9c5139cedebfbc3edec1e1c213cf60442629d7
Austin Zaid Karimi <109770150+Austinzkarimi@users.noreply.github.com>
2024-10-19 07:52:55 -0500

    moer color updates

--- frontend/code-for-good/src/components/Login.tsx
+++ frontend/code-for-good/src/components/Login.tsx
@@ -67,48 +67,59 @@ const LogIn = () => {
   return (
     <>
       <Navbar />
-      <Container maxW="container.xl" height="calc(100vh - 60px)">
-        <Box
-          height="100%"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-        >
+      <Box
+        width="100%"
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        bg="gray.100"
+        padding="2rem"
+      >
+        <Container maxW="container.xl" height="calc(100vh - 60px)">
           <Box
-            width="400px"
-            padding="2rem"
-            boxShadow="lg"
-            bg="white"
-            borderRadius="md"
+            height="100%"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
           >
-            <VStack spacing={6} align="stretch">
-              <Text fontSize="3xl" fontWeight="bold" textAlign="center">
-                Login
-              </Text>
-              <Input
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-              <Input
-                placeholder="Password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button bg="#002982" color = "white" onClick={handleSubmit}>
-                Log In
-              </Button>
-              <Text fontSize="sm" textAlign="center">
-                Don't have an account?{" "}
-                <Link to="/signup_redirect">
-                  <ChakraLink color="#002982">Sign Up</ChakraLink>
-                </Link>
-              </Text>
-            </VStack>
+            <Box
+              width="400px"
+              padding="2rem"
+              boxShadow="lg"
+              bg="white"
+              borderRadius="md"
+            >
+              <VStack spacing={6} align="stretch">
+                <Text fontSize="3xl" fontWeight="bold" textAlign="center">
+                  Login
+                </Text>
+                <Input
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button bg="#002982" color = "white" onClick={handleSubmit}>
+                  Log In
+                </Button>
+                <Text fontSize="sm" textAlign="center">
+                  Don't have an account?{" "}
+                  <Link to="/signup_redirect">
+                    <ChakraLink color="#002982">Sign Up</ChakraLink>
+                  </Link>
+                </Text>
+              </VStack>
+            </Box>
           </Box>
-        </Box>
-      </Container>
+        </Container>
+      </Box>
     </>
   );
 };
--- frontend/code-for-good/src/components/SignUpButtons.tsx
+++ frontend/code-for-good/src/components/SignUpButtons.tsx
@@ -15,7 +15,7 @@ const SignUpButtons = () => {
       <Navbar />
       <Box
         width="100%"
-        height="80vh"
+        height="100vh"
         display="flex"
         justifyContent="center"
         alignItems="center"
