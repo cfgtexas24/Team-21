aab908e499fabaaa6ce36ca2b46af2ae4a8c6ec6
Austin Zaid Karimi <109770150+Austinzkarimi@users.noreply.github.com>
2024-10-19 04:16:56 -0500

    fixed navbar issue with lessons

--- frontend/code-for-good/src/components/lessons/Lesson.tsx
+++ frontend/code-for-good/src/components/lessons/Lesson.tsx
@@ -1,4 +1,4 @@
-import Navbar from "../Navbar";
+import Navbar2 from "../Navbar2";
 import { Question, Resource } from "@/types/Types";
 import { Text } from "@chakra-ui/react";
 import Quiz from "./Quiz";
@@ -12,7 +12,7 @@ interface LessonProps {
 const Lesson = ({ topic, questions, resources }: LessonProps) => {
   return (
     <div className="">
-      <Navbar />
+      <Navbar2 />
       <div className="container h-screen">
         <Text fontSize="xl" className="flex justify-center p-5">
           {topic}
