cd1cc0613822ac4ca17a4ee313935592e1d22ffd
Austin Zaid Karimi <109770150+Austinzkarimi@users.noreply.github.com>
2024-10-18 22:33:54 -0500

    fixed navbar issue

--- frontend/code-for-good/src/App.tsx
+++ frontend/code-for-good/src/App.tsx
@@ -7,12 +7,10 @@ import Mentor from "./components/Mentor";
 import Mentee from "./components/Mentee";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route index element={<Home />} />
         <Route path="/" element={<Home />} />
--- frontend/code-for-good/src/components/Home.tsx
+++ frontend/code-for-good/src/components/Home.tsx
@@ -1,27 +1,29 @@
 import { Link } from "react-router-dom";
 import { Box, Button, Text, VStack } from "@chakra-ui/react";
-
+import "../index.css";
 const Home = () => {
   return (
-    <>
-      <Box paddingTop="4.5rem">
-        <VStack spacing={6}>
-          <Text fontSize="3xl" textDecoration="underline">
-            STORM
-          </Text>
-          <Box>
-            <Link to="/login">
-              <Button>Login / Signup</Button>
-            </Link>
-          </Box>
-          <Box>
-            <Link to="/housing_form">
-              <Button>Emergency Housing Form</Button>
-            </Link>
-          </Box>
-        </VStack>
-      </Box>
-    </>
+    <div className="home-background">
+      <div className="home-content flex flex-col gap-6">
+        <Box paddingTop="4.5rem">
+          <VStack spacing={6}>
+            <Text fontSize="3xl" textDecoration="underline">
+              STORM
+            </Text>
+            <Box>
+              <Link to="/login">
+                <Button>Login / Signup</Button>
+              </Link>
+            </Box>
+            <Box>
+              <Link to="/housing_form">
+                <Button>Emergency Housing Form</Button>
+              </Link>
+            </Box>
+          </VStack>
+        </Box>
+      </div>
+    </div>
   );
 };
 
