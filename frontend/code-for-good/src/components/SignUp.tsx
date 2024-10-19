23b12a444a384cd9aa635403b7379f3db0074e9e
Ashley <ashleyoftexas@gmail.com>
2024-10-19 00:08:07 -0500

    fixing form formatting and package.json

--- frontend/code-for-good/src/components/SignUp.tsx
+++ frontend/code-for-good/src/components/SignUp.tsx
@@ -32,86 +32,91 @@ function SignUp() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
-      <p className="text-3xl">Sign Up</p>
-      <FormControl className="grid grid-cols-12 p-3 gap-x-1 gap-y-3">
-        <FormControl className="col-span-6">
-          <FormLabel>First Name</FormLabel>
-          <Input type="text" aria-label="first name" />
-        </FormControl>
+    <div className="flex justify-center items-center m-10">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col w-1/2 justify-center items-center"
+      >
+        <p className="text-3xl">Sign Up</p>
+        <FormControl className="grid grid-cols-12 p-3 gap-x-1 gap-y-3">
+          <FormControl className="col-span-6">
+            <FormLabel>First Name</FormLabel>
+            <Input type="text" aria-label="first name" />
+          </FormControl>
 
-        <FormControl className="col-span-6">
-          <FormLabel>Last Name</FormLabel>
-          <Input type="text" aria-label="last name" />
-        </FormControl>
+          <FormControl className="col-span-6">
+            <FormLabel>Last Name</FormLabel>
+            <Input type="text" aria-label="last name" />
+          </FormControl>
 
-        <FormControl className="col-span-12">
-          <FormLabel>Age</FormLabel>
-          <NumberInput max={28} min={14}>
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-        </FormControl>
+          <FormControl className="col-span-12">
+            <FormLabel>Age</FormLabel>
+            <NumberInput max={28} min={14}>
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </FormControl>
 
-        <FormControl className="col-span-12">
-          <FormLabel>Gender</FormLabel>
-          <Select placeholder="Select Gender">
-            <option>Man</option>
-            <option>Woman</option>
-            <option>Other</option>
-          </Select>
-        </FormControl>
+          <FormControl className="col-span-12">
+            <FormLabel>Gender</FormLabel>
+            <Select placeholder="Select Gender">
+              <option>Man</option>
+              <option>Woman</option>
+              <option>Other</option>
+            </Select>
+          </FormControl>
 
-        <FormControl className="col-span-12">
-          <FormLabel>Email</FormLabel>
-          <Input
-            placeholder="example@email.com"
-            type="text"
-            aria-label="last name"
-          />
-        </FormControl>
+          <FormControl className="col-span-12">
+            <FormLabel>Email</FormLabel>
+            <Input
+              placeholder="example@email.com"
+              type="text"
+              aria-label="last name"
+            />
+          </FormControl>
 
-        <FormControl className="col-span-12">
-          <FormLabel>Phone Number</FormLabel>
-          <Input
-            placeholder="+1 (123)-456-7890"
-            type="text"
-            aria-label="last name"
-          />
-        </FormControl>
+          <FormControl className="col-span-12">
+            <FormLabel>Phone Number</FormLabel>
+            <Input
+              placeholder="+1 (123)-456-7890"
+              type="text"
+              aria-label="last name"
+            />
+          </FormControl>
 
-        <FormControl className="col-span-6">
-          <FormLabel>City</FormLabel>
-          <Input type="text" aria-label="last name" />
-        </FormControl>
+          <FormControl className="col-span-6">
+            <FormLabel>City</FormLabel>
+            <Input type="text" aria-label="last name" />
+          </FormControl>
 
-        <FormControl className="col-span-6">
-          <FormLabel>State</FormLabel>
-          <Input type="text" aria-label="last name" />
-        </FormControl>
+          <FormControl className="col-span-6">
+            <FormLabel>State</FormLabel>
+            <Input type="text" aria-label="last name" />
+          </FormControl>
 
-        <FormControl className="col-span-12 space-x-1">
-          <FormLabel>Zip Code</FormLabel>
-          <Input type="text" aria-label="last name" />
-        </FormControl>
+          <FormControl className="col-span-12 space-x-1">
+            <FormLabel>Zip Code</FormLabel>
+            <Input type="text" aria-label="last name" />
+          </FormControl>
 
-        <FormControl className="col-span-6">
-          <FormLabel>Housing Status</FormLabel>
-          <RadioGroup defaultValue="Unsheltered">
-            <HStack spacing="24px">
-              <Radio value="Unsheltered">Unsheltered</Radio>
-              <Radio value="Sheltered">Sheltered</Radio>
-            </HStack>
-          </RadioGroup>
+          <FormControl className="col-span-6">
+            <FormLabel>Housing Status</FormLabel>
+            <RadioGroup defaultValue="Unsheltered">
+              <HStack spacing="24px">
+                <Radio value="Unsheltered">Unsheltered</Radio>
+                <Radio value="Sheltered">Sheltered</Radio>
+              </HStack>
+            </RadioGroup>
+          </FormControl>
         </FormControl>
-      </FormControl>
-      <Button mt={4} isLoading={isSubmitting} type="submit">
-        Submit
-      </Button>
-    </form>
+        <Button mt={4} isLoading={isSubmitting} type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
   );
 }
 
